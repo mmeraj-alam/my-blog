6d58c6034949f6446d3b42fd68384f29910e5ab2
mmeraj-alam <mmeraj.alam83@gmail.com>
2024-07-24 23:28:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const articleTitle = document.querySelector('article h2');
-    articleTitle.addEventListener('click', function() {
-        alert('You clicked the blog post title!');
-    });
-});
